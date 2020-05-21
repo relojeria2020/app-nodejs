3426e48a74502218ebed85d5d093b294f49ada73
relojeria2020 <65697914+relojeria2020@users.noreply.github.com>
2020-05-21 07:31:46 -0500

    correccion del registro cliente 2

--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,17 @@
 module.exports = {
- /*
+ 
      database: {
          host: 'bqwwccgaqnx2tespzgmk-mysql.services.clever-cloud.com',
          user: 'uybdomxadqhb9zcb',
          password: 'f88EHLfMZXQ50nfORSH5',
          database: 'bqwwccgaqnx2tespzgmk'
-    }*/
+    }/*
    
      database: {
          host: 'localhost',
          user: 'root',
          password: '',
          database: 'relojeria'
-    }
+    }*/
 
 };
